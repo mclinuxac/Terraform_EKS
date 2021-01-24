cc9a9c4fe8c3136799e831ee57e9757f7e8a819e
monika <monchak@gmail.com>
2021-01-24 11:09:20 -0500

    test

--- main.tf
+++ main.tf
@@ -125,14 +125,5 @@ module "eks" {
 
   vpc_id = module.vpc.vpc_id
 
-  worker_groups = [
-    {
-      name                          = "worker-group-1"
-      instance_type                 = "t2.nano"
-      additional_userdata           = "echo foo bar"
-      asg_desired_capacity          = 2
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-    }
-    ]
 
 }
