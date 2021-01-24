23fac2836377ad58438b4a99f4ae803bfce4bd45
monika <monchak@gmail.com>
2021-01-24 10:58:58 -0500

    test

--- main.tf
+++ main.tf
@@ -108,15 +108,7 @@ module "vpc" {
   single_nat_gateway   = true
   enable_dns_hostnames = true
 
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = "1"
-  }
 
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = "1"
-  }
 }
 
 module "eks" {
