06624e14fc825fbc34b88a0bcf123b349d5bfe3c
monika <monchak@gmail.com>
2021-01-24 08:47:54 -0500

    test

--- main.tf
+++ main.tf
@@ -125,6 +125,7 @@ module "vpc" {
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   cluster_name    = local.cluster_name
+  cluster_version = "1.17"
   subnets         = module.vpc.private_subnets
 
   tags = {
