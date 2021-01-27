7f98c8a1705454daf4c229a0f7d617e5b6dc4f72
monika <monchak@gmail.com>
2021-01-26 19:28:14 -0500

    test

--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.12",
   backend “s3” {
-	bucket = “demo-fugue-opa-0123”
+	bucket = “demo-fugue-opa-0123”,
 	region = “us-east-1”
   }
 }
