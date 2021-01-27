0afeb542162218ecadb211e5efe12ac863256dba
monika <monchak@gmail.com>
2021-01-26 19:23:05 -0500

    test

--- providers.tf
+++ providers.tf
@@ -1,15 +1,14 @@
 terraform {
   required_version = ">= 0.12"
+  backend “s3” {
+	bucket = “demo-fugue-opa-0123”
+	region = “us-east-1”
+  }
 }
 
 provider "aws" {
   region = var.aws_region
 }
 
-# data "aws_availability_zones" "available" {}
+data "aws_availability_zones" "available" {}
 
-# Not required: currently used in conjunction with using
-# icanhazip.com to determine local workstation external IP
-# to open EC2 Security Group access to the Kubernetes cluster.
-# See workstation-external-ip.tf for additional information.
-provider "http" {}
